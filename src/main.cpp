aba94db0d58eec025f3c17bc15d8e2b318d425d4
manjian <manjian2006@gmail.com>
2024-09-20 23:11:49 +0800

    Add the case use std::async.
    Use a new thread to calculate the number and notify
    the asio in the main thread the value.

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,43 @@ awaitable<int> delayed_random_number(boost::asio::io_context& io) {
   co_return result;
 }
 
+class initiate_async_num {
+ public:
+  explicit initiate_async_num() {}
+
+  template <typename CompleteHandler>
+  void operator()(CompleteHandler&& handler) {
+    (void)std::async(std::launch::async,
+                     [my_handler = std::move(handler)]() mutable {
+                       std::random_device rd;
+                       std::mt19937 gen(rd());
+                       std::uniform_int_distribution<> distrib(1, 100);
+                       int result = distrib(gen);
+                       std::move(my_handler)(result);
+                     });
+  }
+};
+
+template <typename Executor = boost::asio::any_io_executor>
+struct AsyncNum {
+  typedef Executor executor_type;
+
+  template <BOOST_ASIO_COMPLETION_TOKEN_FOR(void(int)) ReadToken =
+                boost::asio::default_completion_token_t<executor_type>>
+  auto async_num(ReadToken&& token =
+                     boost::asio::default_completion_token_t<executor_type>())
+      -> decltype(async_initiate<ReadToken, void(int)>(
+          std::declval<initiate_async_num>(),
+          token)) {
+    return async_initiate<ReadToken, void(int)>(initiate_async_num(), token);
+  }
+};
+
+awaitable<int> delayed_random_number_async(boost::asio::io_context& io) {
+  AsyncNum async_num;
+  co_return co_await async_num.async_num(boost::asio::use_awaitable);
+}
+
 int main() {
   boost::asio::io_context io_context;
 
@@ -35,6 +72,19 @@ int main() {
                }
              } else {
                std::cout << "Random number: " << result << '\n';
+             }
+           });
+
+  co_spawn(io_context, delayed_random_number_async(std::ref(io_context)),
+           [](std::exception_ptr e, int result) {
+             if (e) {
+               try {
+                 std::rethrow_exception(e);
+               } catch (const std::exception& ex) {
+                 std::cerr << "Caught exception: " << ex.what() << '\n';
+               }
+             } else {
+               std::cout << "Random number async: " << result << '\n';
              }
            });
 
