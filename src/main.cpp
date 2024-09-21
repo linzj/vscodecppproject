0ed3e1cb35930bbea8cbb67e24351b87c8b01045
manjian <manjian2006@gmail.com>
2024-09-21 09:07:10 +0800

    Make sure the handler call on the correct thread.

--- src/main.cpp
+++ src/main.cpp
@@ -21,40 +21,56 @@ awaitable<int> delayed_random_number(boost::asio::io_context& io) {
   co_return result;
 }
 
-class initiate_async_num {
+template <typename Executor = boost::asio::any_io_executor>
+class InitiateAsyncNum {
  public:
-  explicit initiate_async_num() {}
+  typedef Executor executor_type;
+  explicit InitiateAsyncNum(Executor executor) : executor_(executor) {}
 
   template <typename CompleteHandler>
   void operator()(CompleteHandler&& handler) {
     (void)std::async(std::launch::async,
-                     [my_handler = std::move(handler)]() mutable {
+                     [my_handler = std::move(handler),
+                      my_executor = std::move(executor_)]() mutable {
                        std::random_device rd;
                        std::mt19937 gen(rd());
                        std::uniform_int_distribution<> distrib(1, 100);
                        int result = distrib(gen);
-                       std::move(my_handler)(result);
+                       boost::asio::dispatch(
+                           my_executor,
+                           [result, handler = std::move(my_handler)]() mutable {
+                             std::move(handler)(result);
+                           });
                      });
   }
+
+  executor_type executor_;
 };
 
 template <typename Executor = boost::asio::any_io_executor>
 struct AsyncNum {
   typedef Executor executor_type;
 
+  explicit AsyncNum(executor_type executor) : executor_(executor) {}
+  ~AsyncNum() = default;
+
   template <BOOST_ASIO_COMPLETION_TOKEN_FOR(void(int)) ReadToken =
                 boost::asio::default_completion_token_t<executor_type>>
   auto async_num(ReadToken&& token =
                      boost::asio::default_completion_token_t<executor_type>())
       -> decltype(async_initiate<ReadToken, void(int)>(
-          std::declval<initiate_async_num>(),
+          std::declval<InitiateAsyncNum<Executor>>(),
           token)) {
-    return async_initiate<ReadToken, void(int)>(initiate_async_num(), token);
+    return async_initiate<ReadToken, void(int)>(
+        InitiateAsyncNum<Executor>(executor_), token);
   }
+
+  executor_type executor_;
 };
 
 awaitable<int> delayed_random_number_async(boost::asio::io_context& io) {
-  AsyncNum async_num;
+  auto executor = co_await boost::asio::this_coro::executor;
+  AsyncNum async_num(executor);
   co_return co_await async_num.async_num(boost::asio::use_awaitable);
 }
 
